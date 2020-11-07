5c876c66097e1de10732039e0b60900a03ca8e53
mikejiang <wjiang2@fhcrc.org>
2020-11-06 17:27:10 -0800

    cyto_pkg_deps & cyto_remote

---
+++ R/deps.R
@@ -0,0 +1,52 @@
+options("cyto_repo_owner" = "mikejiang")
+options("cyto_repos" = c("ggcyto", "flowWorkspace"))
+#' @param pkg the package name
+#' @param owner the github owner
+#' @param ... arguments passed to remotes::dev_package_deps
+#' @importFrom remotes dev_package_deps
+#' @importFrom dplyr rowwise mutate %>%
+#' @export
+#' @examples
+#' cyto_pkg_deps("ggcyto")
+cyto_pkg_deps <- function(pkg, owner = getOption("cyto_repo_owner"), ...)
+{
+  #download DESC file
+  pkdir <- tempfile()
+  dir.create(pkdir)
+  bioc_ver = bioc_version()
+  branch <- paste0("bioc_", bioc_ver)
+  url <- file.path("https://raw.githubusercontent.com", owner, pkg, branch, "DESCRIPTION")
+  download.file(url, file.path(pkdir, "DESCRIPTION"), quiet = TRUE)
+  #get deps from public repos (cran, bioc)
+  deps <- dev_package_deps(pkdir, ...)
+  #update it with github release version (when it is higher version)
+  cyto_repos <- getOption("cyto_repos")
+  for(i in seq_len(nrow(deps)))
+  {
+    package <- deps[i, ][["package"]]
+    if(package %in% cyto_repos)
+    {
+      # browser()
+      remote <- deps[i, ][["remote"]][[1]]
+
+      #compare the public remote with cytorepo
+      remote_new <- cyto_remote(package) #fetch pkg info from cyto remote repo
+      ver <- remote_sha(remote_new)
+      diff1 <- remotes:::compare_versions(remote_sha(remote), ver, is_cran = TRUE)
+      #replace the public remote by cyto remote when it is behind
+      if(diff1 %in% c(remotes:::BEHIND, remotes:::UNINSTALLED))
+      {
+        deps[i, "available"] = ver
+        deps[i, "diff"] = diff1
+        deps[i, "is_cran"] = FALSE
+        deps[i, ][["remote"]][[1]] = remote_new
+      }
+    }
+
+  }
+  deps
+}
+
+
+
+
---
+++ R/install_cyto.R
@@ -0,0 +1,114 @@
+
+#' Attempts to install a package from cyto repo
+#'
+#' This function is vectorised on `pkgs` so you can install multiple
+#' packages in a single command.
+#'
+#' @param pkgs Character vector of packages to install.
+#' @param owner the github owner
+#' @export
+#' @family package installation
+#' @examples
+#' \dontrun{
+#' install_cyto("ggcyto")
+#' }
+install_cyto <- function(pkgs, owner = getOption("cyto_repo_owner"), type = getOption("pkgType"),
+                         dependencies = NA,
+                         upgrade = c("default", "ask", "always", "never"),
+                         force = FALSE,
+                         quiet = FALSE,
+                         build = TRUE, build_opts = c("--no-resave-data", "--no-manual", "--no-build-vignettes"),
+                         build_manual = FALSE, build_vignettes = FALSE,
+                         ...) {
+
+  remotes <- lapply(pkgs, cyto_remote, owner = owner
+                    , type = type
+                    )
+
+  remotes:::install_remotes(remotes,
+                  dependencies = dependencies,
+                  upgrade = upgrade,
+                  force = force,
+                  quiet = quiet,
+                  build = build,
+                  build_opts = build_opts,
+                  build_manual = build_manual,
+                  build_vignettes = build_vignettes,
+                  repos = repos,
+                  type = type,
+                  ...)
+}
+
+#' construct a 'cyto_remote' object
+#'
+#' It queries the remote github repo and fetch the packager versions and download url
+#' @param pkg package name
+#' @param owner github repo owner
+#' @export
+#' @examples
+#' remote <- cyto_remote("ggcyto")
+#' remote_package_name(remote)
+cyto_remote <- function(pkg, owner = getOption("cyto_repo_owner"), type = getOption("pkgType"), ...) {
+
+  res <- cyto_pkg_github_url(pkg, owner)
+  res <- remotes:::remote("cran",
+         name = pkg,
+         repo = NULL,
+         ver = res[["ver"]],
+         url = res[["url"]],
+         pkg_type = type)
+  class(res) <- c("cyto_remote", class(res))
+  res
+}
+
+#' @importFrom remotes remote_package_name
+#' @export
+remote_package_name.cyto_remote <- function(remote, ...) {
+  remote$url
+}
+#' @importFrom remotes remote_sha
+#' @export
+remote_sha.cyto_remote <- function(remote, ...) {
+  remote$ver
+
+}
+
+#' @export
+format.cyto_remote <- function(x, ...) {
+  "CYTO"
+}
+
+#' get the download url of the package hosted as github release assets
+#'
+#' @param pkg the package name
+#' @param owner the github owner
+#' @param bioc_ver the corresponding bioconductor version
+#' @importFrom gh gh
+#' @importFrom remotes bioc_version
+#' @examples
+#' cyto_pkg_github_url("ggcyto")
+cyto_pkg_github_url <- function(pkg, owner = getOption("cyto_repo_owner"), bioc_ver = bioc_version())
+{
+  releaseid <- gh("GET /repos/:owner/:repo/releases/tags/:tag", owner = owner, repo = pkg, tag = paste0("bioc_",bioc_ver))$id
+  assets <- gh("GET /repos/:owner/:repo/releases/:release_id/assets", owner = owner, repo = pkg, release_id = releaseid)
+  suffix <- switch(Sys.info()[['sysname']],
+                   Linux = "tar\\.gz",
+                   Darwin = "tgz",
+                   Windows = "zip"
+  )
+  pkg_bin <- paste0("^", pkg, "_[0-9\\.]+\\.", suffix, "$")
+  asset.names <- vapply(assets, "[[", "", "name")
+  idx <- grepl(pkg_bin, asset.names)
+  nfiles <- sum(idx)
+  if(nfiles == 0)
+    return(NA)
+  else if(nfiles > 1)
+  {
+    return(NA)#todo: maybe get the latest version
+  }else
+
+    asset <- assets[[which(idx)]]
+
+  ver <- sub(paste0("^", pkg, "_"), "", sub(paste0("\\.", suffix, "$"), "",  asset[["name"]]))
+  list(ver = ver, url = asset[["browser_download_url"]])
+}
