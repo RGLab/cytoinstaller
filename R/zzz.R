650174261d97a89adc2981542b58e9f4805225f9
mikejiang <wjiang2@fhcrc.org>
2020-11-14 17:27:21 -0800

    up

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname){
-  options("cyto_repo_owner" = "mikejiang")
+  options("cyto_repo_owner" = "RGLab")
   options("cyto_repos" = c("cytolib",  "flowCore", "openCyto", "flowStats", "flowClust", "ggcyto", "flowWorkspace"))
 
 }
